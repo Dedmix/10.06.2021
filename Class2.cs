fc89f3c2c9144b4685fadfaf8eb6066a826aa7d6
Dedmix <84181853+Dedmix@users.noreply.github.com>
2021-06-10 14:01:17 +0500

    Add files via upload

---
+++ Class2.cs
@@ -0,0 +1,194 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class MyList<T>
+
+	{
+		Node<T> firstElement;
+
+		Node<T> GetLast()
+		{
+			Node<T> last = firstElement;
+
+			while (last.next != null)
+			{
+				last = last.next;
+			}
+			return last;
+		}
+		public T Get(int i)
+		{
+			Node<T> last = firstElement;
+			for (int j = 0; j < i; j++)
+			{
+				last = last.next;
+			}
+			return last.value;
+		}
+		public void Print()
+		{
+			Node<T> last = GetFirst();
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.next;
+
+			}
+		}
+		public void AddFront(T value)//добавление в начале списка
+		{
+			Node<T> abc = new Node<T>();
+			abc.value = value;
+
+			if (firstElement == null)
+			{
+				firstElement = abc;
+			}
+			else
+			{
+				abc.next = GetFirst();
+				abc.next.prev = abc;
+			}
+		}
+		public void AddBack(T value)//добавление в конце списка 
+		{
+			Node<T> abc = new Node<T>();
+			abc.value = value;
+
+			if (firstElement == null)
+			{
+				firstElement = abc;
+			}
+			else
+			{
+				abc.prev = GetLast();
+				GetLast().next = abc;
+
+
+			}
+		}
+		Node<T> GetFirst()
+		{
+			Node<T> last = firstElement;
+
+			while (last.prev != null)
+			{
+				last = last.prev;
+			}
+			return last;
+		}
+		int Count()
+		{
+			int i = 0;
+			Node<T> last = firstElement;
+			while (last.next != null)
+			{
+				i++;
+				last = last.next;
+			}
+			return i;
+		}
+		public void Search(int i)//копирование скроки 
+		{
+			if ((i != 0) && (i <= Count()))
+			{
+				Node<T> last = firstElement;
+
+				for (int j = 0; j < i; j++)
+				{
+					last = last.next;
+				}
+				Console.WriteLine(last.value);
+			}
+			else throw new IndexOutOfRangeException();
+		}
+		public void Remove(int i)
+		{
+			Node<T> last = firstElement;
+			Node<T> OneAhead;
+			for (int j = 0; j < i; j++)
+			{
+				last = last.next;
+			}
+			OneAhead = last.next;
+			last = last.prev;
+			last.next = OneAhead;
+		}
+		public void InsertBefore(int i, T C)
+		{
+			Node<T> last = firstElement;
+			Node<T> ded1 = new Node<T>();
+			Node<T> ded2 = last.prev;
+			for (int j = 0; j < i; j++)
+			{
+				last = last.next;
+			}
+			ded1.value = C;
+			ded1.prev = ded2;
+			ded1.next = last;
+			ded2.next = ded1;
+			last.prev = ded1;
+		}
+		public void InsertAfter(int i, T C)
+		{
+			Node<T> last = firstElement;
+			Node<T> ded1 = new Node<T>();
+			Node<T> ded2 = last.next;
+
+			ded1.value = C;
+			ded1.next = ded2;
+			ded1.prev = last;
+			ded2.prev = ded1;
+			last.next = ded1;
+		}
+		public void PopFront()
+		{
+			Node<T> ded = firstElement;
+			firstElement = ded;
+			ded.next = firstElement.next;
+			ded.prev = firstElement.prev;
+			firstElement = ded;
+		}
+		public void PopBack()
+		{
+			Node<T> ded = firstElement;
+			firstElement = ded;
+			ded.next = firstElement.next;
+			ded.prev = firstElement.prev;
+			firstElement = ded;
+		}
+		public void PrintRevers()
+		{
+			Node<T> last = GetLast();
+			while (last != null)
+			{
+				Console.WriteLine(last.value);
+				last = last.prev;
+
+			}
+		}
+		public int Search3(int i, T C)
+		{
+			{
+				Node<T> last = firstElement;
+				int index = -1;
+				while (last != null)
+				{
+					index++;
+					if (last.next == last)
+					{
+						return index;
+					}
+				}
+				last = last.next;
+				return -1;
+			}
+		}
+	
+	}
+}
---
+++ Program.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApp1
+{
+	class Program
+	{
+		static void Main(string[] args)
+		{
+			MyList<string> list = new MyList<string>();
+			list.AddBack("Безумие это точное повторение одного и того же действия раз за разом,");
+			list.AddBack("В надежде на изменнение это и безумие ");
+			list.AddBack("Когда впервые я это услышал, не помню кто сказал эту хрень я БУМ, убил его");
+			list.AddFront("Я уже говорил что такое безумиею А?");
+			list.AddBack("Смысл в том хахаха Окей ? Он был прав.");
+			list.AddBack("Куда ни глянь, делают точно одно и тоже, сона и снова, и снова, и снова");
+			list.AddBack("И думают: Сейчас все исменить");
+			list.AddBack("Не-не-не, прошу. Сейчас все будет иначе.");
+
+			list.Remove(2);
+		    list.InsertBefore(2, "Когда впервые я это услышал, не помню кто сказал эту хрень я БУМ, убил его. (добавил)");
+		    list.InsertAfter(4, "Везде, куда ни гляну - эти болваны. (доб)");
+			list.Print();
+			list.Search(1);
+			list.Search3();
+			Console.WriteLine();
+			list.PrintRevers();
+		
+
+
+
+			Console.ReadKey();
+
+
+
+
+
+		}
+	}
+}
